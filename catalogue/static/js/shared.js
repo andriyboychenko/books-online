62e99048aab633719b1375f6bee5302b9bec2990
Andriy <andriy@andriy-N43JM.(none)>
2013-12-01 01:41:25 +0000

    Item remove generic confirmation window. Cleaning inputs when user closes modal window

--- catalogue/static/js/shared.js
+++ catalogue/static/js/shared.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 
     var alertMessageElement = $(".alert");
+    var modalWindow = $('#modify-window');
     
 	/* Modal window of ADD, EDIT */
 	$("#add-btn").click(function(){
-		$('#modify-window').modal();
+		modalWindow.modal();
 		$('.modal-title').text("Добавить "+modal_title);
 	});
+    
+    /* When closing modal window, all input fields should be clean*/
+    modalWindow.on('hidden.bs.modal', function () {
+        
+        $("form").find("input:text,textarea,select").each(function(index){
+            $(this).val("");
+        });
+    });
     
     
     /* save object from modal window*/
@@ -14,7 +23,7 @@ $(document).ready(function() {
         
         var invalidInputs = 0;
         
-        $("form").find('input:text,textarea,select').each(function(index){ 
+        $("form").find("input:text,textarea,select").each(function(index){ 
              
              var currElement = $(this);            
              
@@ -25,7 +34,7 @@ $(document).ready(function() {
              if( currElement.get(0).tagName !== "SELECT"){
                                  
                  var isValid = true;
-                 if(currElement.attr('required') === 'required'){
+                 if(currElement.attr("required") === "required"){
                      isValid = validateInput(this);
                  }
                  if(!isValid){
@@ -40,9 +49,9 @@ $(document).ready(function() {
             return false;
         }
         
-     
-        
 	});
+    
+    
 
 	/* Close aler messages */
 	$(".close-btn").click(function (){
@@ -50,43 +59,55 @@ $(document).ready(function() {
 	});
 
 
-    /* Remove object */
+    /* -------Remove object -----------*/
+    var currentRemoveId;
 	$(".remove-btn").click(function (){
-                
-            alertMessageElement.hide();
         
-            var currentId = $(this).attr('id');
-            
-            $.post( "/ajax_catalogue/remove/", 
-                {
-                    object_id:currentId,
-                    object_type:object_type
-                },   
-                function( data ) {
-                    var response = eval('(' + data + ')');
+        currentRemoveId = $(this).attr('id');
 
-                    if(response.status === 1){
+        alertMessageElement.hide();
+            
+        $(".remove-object-name").html(
+            $("#remove-confirmation-object-name").val()
+        );
+        $("#remove-confirmation-window").modal();        
+            
+        
+    });
+    
+    $("#remove-confirm-object").click(function (){
+            
+        $.post( "/ajax_catalogue/remove/", 
+            {
+                object_id:currentRemoveId,
+                object_type:object_type
+            },   
+            function( data ) {
+                var response = eval('(' + data + ')');
+
+                if(response.status === 1){
+                                        
+                    $("#row_"+currentRemoveId)
+                        .animate( {backgroundColor:'#E8E8E8'}, 500)
+                            .fadeOut(500,function() {
+                                $('#row_'+currentRemoveId).remove();
+                    });
                                             
-                        $("#row_"+currentId)
-                            .animate( {backgroundColor:'#E8E8E8'}, 1000)
-                                .fadeOut(1000,function() {
-                                    $('#row_'+currentId).remove();
-                        });
-                                                
-                        $("#super-cathegory-select option[id='cathegory_option_"+currentId+"']").remove();
-                        
-                        $("#info-message > span").html(success_msg);
-                        $("#info-message").show();
-                        
-                    }else {
-                        
-                        $("#error-message > span").html(error_msg);
-                        $("#error-message").show();
+                    $("#super-cathegory-select option[id='cathegory_option_"+currentRemoveId+"']").remove();
                     
-                    }
+                    $("#info-message > span").html(success_msg);
+                    $("#info-message").show();
+                    
+                }else {
+                    
+                    $("#error-message > span").html(error_msg);
+                    $("#error-message").show();
+                
                 }
-            );
-      });
+            }
+        );
+        
+    });
             
 
 });
